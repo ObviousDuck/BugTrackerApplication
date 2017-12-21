740bf099c0ecc06402ce47ebdd78c8f76da2a039
Andreas Andresen <andreas18dk@gmail.com>
2017-12-21 13:36:42 +0000

    Created the form template for the application with buttons which will eventually control the level of access to the application.

--- BugTrackerApplication/Form1.Designer.cs
+++ BugTrackerApplication/Form1.Designer.cs
@@ -73,7 +73,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(162, 223);
+            this.button2.Location = new System.Drawing.Point(162, 217);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(205, 37);
             this.button2.TabIndex = 3;
